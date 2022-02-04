3350ebcbe227efc8d68cbd3495e4ab963a3a81fe
dev-damdam <dev.sodam@gmail.com>
2022-02-05 03:27:04 +0900

    impl firebase email password login and check email verify

--- app/src/main/java/com/example/healingmessage/account/LoginActivity.kt
+++ app/src/main/java/com/example/healingmessage/account/LoginActivity.kt
@@ -1,9 +1,14 @@
 package com.example.healingmessage.account
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.healingmessage.MainActivity
 import com.example.healingmessage.R
 import com.example.healingmessage.databinding.ActivityLoginBinding
+import com.example.healingmessage.firebase.FirebaseBaseSingleton
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -12,11 +17,14 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setButtonClickEventListener()
     }
 
     private fun setButtonClickEventListener() {
         binding.buttonLogin.setOnClickListener {
-           //login check
+            //login
+            login()
         }
 
         binding.textviewFindEmail.setOnClickListener {
@@ -29,6 +37,22 @@ class LoginActivity : AppCompatActivity() {
 
         binding.textviewRegister.setOnClickListener {
             //intent register activity
+        }
+    }
+
+    private fun login() {
+        val res = FirebaseBaseSingleton.getInstance().login(this, "dev.sodam@gmail.com", "test123!")
+
+        res?.addOnCompleteListener { task ->
+            if(task.isSuccessful) {
+                if(FirebaseBaseSingleton.getInstance().checkEmailVerified() == true) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NO_ANIMATION
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "이메일 인증을 해주세요.", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/healingmessage/firebase/FirebaseBaseSingleton.kt
@@ -0,0 +1,93 @@
+package com.example.healingmessage.firebase
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.util.*
+
+class FirebaseBaseSingleton {
+    companion object {
+        private var instance: FirebaseBaseSingleton? = null
+        //database
+        private var database: FirebaseDatabase? = null
+        private var databaseReference: DatabaseReference? = null
+
+        //auth
+        private var auth: FirebaseAuth? = null
+
+        init {
+            init()
+        }
+
+        fun getInstance(): FirebaseBaseSingleton {
+            if (instance == null) {
+                instance = FirebaseBaseSingleton()
+            }
+
+            return instance!!
+        }
+
+        private fun init() {
+            if (database == null) {
+                database = FirebaseDatabase.getInstance()
+            }
+
+            if (databaseReference == null) {
+                databaseReference = database!!.reference
+            }
+
+            if(auth == null) {
+                auth = FirebaseAuth.getInstance()
+            }
+        }
+
+        fun getDatabase(): FirebaseDatabase {
+            if (database == null) {
+                database = FirebaseDatabase.getInstance()
+            }
+
+            return database!!
+        }
+
+        fun getDatabaseReference(): DatabaseReference {
+            if (databaseReference == null) {
+                databaseReference = database!!.reference
+            }
+
+            return databaseReference!!
+        }
+    }
+
+    fun register(context: Context, email: String, password: String) {
+        auth?.createUserWithEmailAndPassword(email, password)
+            ?.addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    auth?.currentUser
+                        ?.sendEmailVerification()
+                        ?.addOnCompleteListener { task ->
+                            if(task.isSuccessful) {
+                                Toast.makeText(context, "회원가입 인증 성공", Toast.LENGTH_SHORT).show()
+                            }
+                            else {
+                                Toast.makeText(context, "회원가입 인증 실패", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                } else {
+                    Toast.makeText(context, "회원가입 실패", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    fun login(context: Context, email: String, password: String): Task<AuthResult>? {
+        return auth?.signInWithEmailAndPassword(email, password)
+    }
+
+    fun checkEmailVerified(): Boolean? {
+        return auth?.currentUser?.isEmailVerified
+    }
+}
